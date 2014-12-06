e5930688f8e4963a3c567c08b094e5af95a6b0f1
rmeji1 <rmeji1@unh.newhaven.edu>
2014-12-05 19:44:58 -0500

    Finished

--- Python Final/robertoMejia.py
+++ Python Final/robertoMejia.py
@@ -14,12 +14,26 @@ def getData():
         print(e)
     return data
 
+#def updateData(request):
+    
+        
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', data = getData());
  
-@app.route('/', methods=['POST'])
+@app.route('/update', methods=['POST'])
 def indexUpdate():
-    
+    try:
+        conn = sqlite3.connect("final_exam.sqlite")
+        cursor = conn.cursor()
+        name = request.form['name']
+        gift = request.form['gift']
+        print(name,gift)
+        cursor.execute('UPDATE gifts SET name = (?) WHERE gift=(?)', (name,gift))
+        conn.commit();
+    except Exception as e:
+        print(type(e))
+        print(e)
     return render_template('index.html', data = getData());
 app.run()
