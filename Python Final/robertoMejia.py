c4fdd508c96b8558ee0b36b4ea903dd3bafc522a
rmeji1 <rmeji1@unh.newhaven.edu>
2014-12-05 19:10:55 -0500

    Added Database connectivity

--- Python Final/robertoMejia.py
+++ Python Final/robertoMejia.py
@@ -3,13 +3,23 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 app = Flask(__name__)
 
+def getData():
+    try:
+        conn = sqlite3.connect("final_exam.sqlite")
+        cursor = conn.cursor()
+        result = cursor.execute('SELECT gift, name from gifts')
+        data = result.fetchall()
+    except Exception as e:
+        print(type(e))
+        print(e)
+    return data
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data = getData());
  
 @app.route('/', methods=['POST'])
 def indexUpdate():
-    conn = sqlite3.connect("final_exam.sql")
-
-    return render_template('index.html', data = data);
+    
+    return render_template('index.html', data = getData());
 app.run()
